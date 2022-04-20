34363022f4211b7070937d463e0241b6f5c7a40c
ArtemMaluev <artem.rus13@yandex.ru>
2022-04-20 23:41:53 +0300

    Initial commit

---
+++ src/maluevArtem/Main.java
@@ -0,0 +1,12 @@
+package maluevArtem;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinOps bins = new BinOps();
+        System.out.println(bins.mult("101", "10")); // 5, 2
+        System.out.println(bins.sum("1010", "1000")); // 10, 8
+
+    }
+}
